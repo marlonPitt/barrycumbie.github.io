14cfe0023a9a19bdf9ff2f08a0696489d3dbec34
marlonPitt <35789276+marlonPitt@users.noreply.github.com>
2018-03-07 15:37:24 -0600

    Add files via upload

---
+++ mpitt_index.js
@@ -0,0 +1,17 @@
+// my js
+
+
+
+$(document).ready(function(){
+
+    var nodeList = document.body.childNodes;
+
+	for (i=0; i < nodeList.length; i++) {
+		
+		//console.log('in da loop?');
+
+		console.log(document.body.childNodes[i]);
+		
+	};
+	//console.log('out of the loop');
+});
